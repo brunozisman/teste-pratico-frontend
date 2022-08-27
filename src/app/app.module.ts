c7bee08a9580fe028a352ef0a22af9a2169cbfce
brunozisman <brunonzisman@hotmail.com>
2022-08-27 13:29:40 -0300

    User and user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UsersService } from './users.service';
+import {HttpClientModule} from '@angular/common/http'
+import {ReactiveFormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [HttpClientModule, UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/users.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'aaplication/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  url = 'https://localhost:5001/users'
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<User[]>{
+    return this.http.get<User[]>(this.url);
+  }
+
+  findById(userId: any): Observable<User> {
+    const apiUrl = `${this.url}/${userId}`
+    return this.http.get<User>(apiUrl)
+  }
+
+  create(user: User): Observable<any> {
+    return this.http.post<User>(this.url, user, httpOptions)
+  }
+
+  update(user: User): Observable<any> {
+    return this.http.put<User>(this.url, user, httpOptions)
+  }
+
+  delete(userId: any): Observable<any> {
+    const apiUrl = `${this.url}/${userId}`
+    return this.http.delete<any>(apiUrl, httpOptions)
+  }
+}
